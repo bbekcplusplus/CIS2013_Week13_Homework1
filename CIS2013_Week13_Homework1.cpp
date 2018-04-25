95cf8ecd42e3a3b6d98b3c64c36ffc7549b6584e
Bibek Gupta <bibek.gupta@okstate.edu>
2018-04-25 12:31:23 -0500

    updates

--- CIS2013_Week13_Homework1.cpp
+++ CIS2013_Week13_Homework1.cpp
@@ -1,45 +1,153 @@
-#include "Account.cpp"
-/* Main Function start*/
-int main() {
-	float balance;
-	int choice = 0, i = 0, testNumber;
-	Account account[TOTAL_ACCOUNTS];
-	while (true) {
-		/*Maximum 10 account should be created as length of array*/
-		if (i == 10) {
-			break;
-		}
-		account[i].setUserData();
-		account[i].displayAccountInfo();
-		cout << "Do u wana create Another account 1/0: ";
-		cin >> choice;
-		if (choice == 0) {
-			break;
-		}
-		i++;
-	}
+#include<iostream>
+#include <cstdlib>
 
-	cout << "Please Enter Account number to be details displayed: " << endl;
-	cin >> testNumber;
-	for (i = 0; i<TOTAL_ACCOUNTS; i++) {
-		if (account[i].getAccountNumber() == testNumber) {
+#include <ctime>
 
-			/* prompt for depositing money in account*/
-			cout << "Enter the amount to be deposited: " << endl;
-			cin >> balance;
-			account[i].deposit(balance);
-			/* prompt for withdraw money from account*/
-			int flag = 1;
-			while (flag) {
-				cout << "Enter the amount to be withdraw: " << endl;
-				cin >> balance;
-				/* withdraw function calling*/
-				account[i].withdraw(balance);
-				cout << "Do u wana withdraw again 1/0: " << endl;
-				cin >> flag;
-			}
-		}
-	}
-	return 0;
-	getchar();
+using namespace std;
+// Studies Problem Solving with C++ (Course Book)
+// Some online C++ books for example: Chegg (computer science learning materials)
+class BankAccount{
+
+public:
+
+string name;
+
+int account_num;
+
+int phone;
+
+int balance;
+
+double rate;
+
+static int acc_num;
+
+BankAccount(string name,int phone,int balance,double rate)
+
+{
+
+static int acc_num=0;
+
+this->account_num=++acc_num;
+
+this->name=name;
+
+this->phone=phone;
+
+this->balance=balance;
+
+this->rate=rate;
+
 }
+
+void deposit(int amount)
+
+{
+
+this->balance+=amount;
+
+}
+
+void withdraw(int amount)
+
+{
+
+this->balance-=amount;
+
+cout<<"Successfully Withdrawn "<<amount<<endl;
+
+}
+
+int balance()
+
+{
+
+return this->balance;
+
+}
+
+};
+
+class Savings:public BankAccount{
+
+public:
+
+int minimum_balance;
+
+int withdraw_limit;
+
+Savings(string name,int phone,int balance,double rate,int minimum_balance,int withdraw_limit):BankAccount(name,phone,balance,rate)
+
+{
+
+this->minimum_balance=minimum_balance;
+
+this->withdraw_limit=withdraw_limit;
+
+}
+
+void withdraw(int amount)
+
+{
+
+if(amount>this->withdraw_limit)
+
+{
+
+cout<<"Amount greater than withdraw limit!\n";
+
+}
+
+else
+
+{
+
+this->balance-=amount;
+
+cout<<"Successfully Withdrawn "<<amount<<endl;
+
+if(this->balance<this->minimum_balance)
+
+{
+
+cout<<"Balance less than minimum balance, please deposit!\n";
+
+}
+
+}
+
+}
+
+};
+
+class Checking:public BankAccount{
+
+public:
+
+int overdraft_fee;
+
+Checking(string name,int phone,int balance,double rate,int overdraft_fee):BankAccount(name,phone,balance,rate){
+
+this->overdraft_fee=overdraft_fee;
+
+}
+
+void withdraw(int amount)
+
+{
+
+this->balance-=amount;
+
+if(this->balance<0)
+
+{
+
+cout<<"Overdraft fee charged!\n";
+
+this->balance-=this->overdraft_fee;
+
+}
+
+}
+
+};
